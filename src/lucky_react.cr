8638a69d2daad38ae6fc46dd1d1101419d1ae6bb
mikeeus <itsmikias@gmail.com>
2018-07-30 09:33:24 +0300

    Initial commit

---
+++ src/lucky_react.cr
@@ -0,0 +1,6 @@
+require "./lucky_react/*"
+
+# TODO: Write documentation for `LuckyReact`
+module LuckyReact
+  # TODO: Put your code here
+end
---
+++ src/lucky_react/version.cr
@@ -0,0 +1,3 @@
+module LuckyReact
+  VERSION = "0.1.0"
+end
